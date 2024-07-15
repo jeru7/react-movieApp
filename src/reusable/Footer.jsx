29a6c1e7fee707f887ca77cda5648364724d24f5
jeru7 <john.ungab7@gmail.com>
2024-07-15 14:33:57 +0800

    update: github link added

--- src/reusable/Footer.jsx
+++ src/reusable/Footer.jsx
@@ -5,8 +5,15 @@ const Footer = () => {
   return (
     <footer className='flex items-center justify-center w-full p-4 text-whiteText'>
       <div className='flex items-end gap-1'>
-        <FontAwesomeIcon icon={faGithub} className='text-4xl' />
-        <p className=''>jeru7</p>
+        <a
+          href='https://github.com/jeru7'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex items-end gap-1 hover:scale-105 '
+        >
+          <FontAwesomeIcon icon={faGithub} className='text-4xl' />
+          <p className=''>jeru7</p>
+        </a>
       </div>
     </footer>
   )
